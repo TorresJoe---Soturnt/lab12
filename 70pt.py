a7ec1de7fe9c6df5bd1ead17c0e7ed6210a72c19
TorresJoe---Soturnt <s631560@student.roundrockisd.org>
2014-11-04 16:37:35 -0600

    First Commit
    70 pts

--- 70pt.py
+++ 70pt.py
@@ -29,6 +29,12 @@ class MyApp:
 		self.button3.grid(row=0,column=2)	  ### (3)
 			
 	
+		self.button4 = Button(self.myContainer1)
+		self.button4["text"]= "Goodbye!"
+		self.button4["background"] = "yellow"
+		self.button4.grid(row=0,column=3)
+		
+		
 		
 root = Tk()
 myapp = MyApp(root)
